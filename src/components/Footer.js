10e23b2329e445f8e26691a79effafc6e0c26c92
Mowsikan H <727623bea030@mcet.in>
2025-08-14 05:55:41 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Link, Typography } from '@mui/material';
 
 export default function Footer() {
-  const api = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
+  const api = process.env.REACT_APP_API_BASE || 'https://sakthi-hackathon-2.onrender.com/api';
   return (
     <Box sx={{ py: 3, textAlign: 'center', color: 'text.secondary' }}>
       <Typography variant="caption">
